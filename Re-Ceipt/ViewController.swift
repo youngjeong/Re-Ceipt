5d1335c6988604693b8217a7a39429ec1b95eb88
FinnYu <ryj1549@gmail.com>
2017-12-07 18:24:38 +0900

    initial commit

---
+++ Re-Ceipt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Re-Ceipt
+//
+//  Created by 유영정 on 2017. 12. 7..
+//  Copyright © 2017년 SSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
